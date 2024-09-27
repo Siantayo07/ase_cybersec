f0e7681a0f33df3fb1d380daf0bf549b5378c350
Siantayo07 <psiantayo96@gmail.com>
2024-09-27 16:18:27 +0200

    Several times is a charm

--- app/app.py
+++ app/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, make_response, jsonify
 
 app = Flask(__name__, instance_relative_config=True)
+from functools import reduce
+import operator
 
 @app.route('/add')
 def add():
@@ -10,6 +12,69 @@ def add():
         return make_response(jsonify(s=a+b), 200) #HTTP 200 OK
     else:
         return make_response('Invalid input\n', 400) #HTTP 400 BAD REQUEST
+    
+    
+@app.route('/upper')
+def upper():
+    a = request.args.get('a', type=str)
+    if a:
+        return make_response(jsonify(s=a.upper()), 200) 
+    else: 
+        return make_response('Invalid Input\n', 400)
+
+@app.route('/lower')
+def lower():
+    a = request.args.get('a', type=str)
+    if a:
+        return make_response(jsonify(a.lower()), 200) 
+    else: 
+        return make_response('Invalid Input\n', 400)
+    
+@app.route('/concat')
+def concat():
+    a = request.args.get('a', type=str)
+    b = request.args.get('b', type=str)
+    if a and b:
+        return make_response(jsonify(a+b), 200) 
+    else: 
+        return make_response('Invalid Input\n', 400)
+    
+@app.route('/reduce')
+def reduce_op():
+    op = request.args.get('op', type=str)
+    lst = request.args.get('lst', type=str)
+    
+    if not op or not lst:
+        return make_response('Invalid Input\n', 400)
+    try:
+        # Example: 'lst=[2,1,3,4]' will become [2,1,3,4]
+        lst = eval(lst)
+        if not isinstance(lst, list):
+            raise ValueError
+    except:
+        return make_response('Invalid List Format\n', 400)
+
+    if op == "add":
+        result = reduce(operator.add, lst)
+ 
+    elif op == 'sub': 
+        result = reduce(operator.sub, lst)
+        
+    elif op == 'mul': 
+        result = reduce(operator.mul, lst)
+    
+    elif op == 'div': 
+        try:
+            result = reduce(operator.truediv, lst)
+        
+        except ZeroDivisionError:
+            return make_response('Division by zero is not allowed\n', 400)
+             
+    else:
+        return make_response('Invalid Operation\n', 400)
+    
+    return make_response(jsonify(s=result), 200)
+  
 
 #Endpoint /sub for subtraction which takes a and b as query parameters.
 
